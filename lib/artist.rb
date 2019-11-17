6c1252b39ebd186aeeefbe31cbe9943ec3054cc8
twwright <twwright@gmail.com>
2019-11-17 13:19:17 -0600

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,30 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
-  attr_reader :songs
+	extend Memorable, Findable
+	include Paramable, Initialize
 
-  @@artists = []
+	attr_accessor :name
+	attr_reader :songs
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+	@@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+	def initialize
+		super
+		@songs = [] #Creates empty collection of artist's songs
+	end
 
-  def self.all
-    @@artists
-  end
+	def self.all
+		@@artists #Outputs all artist instances
+	end
 
-  def self.reset_all
-    self.all.clear
-  end
+	def add_song(song)
+		@songs << song #Adds a song to the artist instance's song list
+		song.artist = self #Sets the song instance's attribute to the name
+	end
 
-  def self.count
-    self.all.count
-  end
+	def add_songs(songs)
+		songs.each { |song| add_song(song) } #Checks a songlist to see if the artist matches an artist instance, and if it does adds it to the artist's list
+	end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+module Memorable
+
+	def reset_all
+		self.all.clear #Deletes all artist instances
+	end
+
+	def count
+		self.all.count #Counts all artist instances
+	 end
+
+end
